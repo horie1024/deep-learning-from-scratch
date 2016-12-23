8c7c0b465aaf32baf9be4f6480ec9aa1ff3533f8
horie1024 <ryosuke.pic@gmail.com>
2016-12-23 22:29:58 +0900

    Added multidimensional_array.py

---
+++ chapter3/multidimensional_array.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+A = np.array([1, 2, 3, 4])
+
+print(A)
+
+print(np.ndim(A)) # 次元数の取得
+
+print(A.shape) # 配列の形状を取得
+
+print(A.shape[0]) # 
+
+print("--------------")
+
+B = np.array([[1, 2], [3, 4], [5, 6]])
+
+print(B)
+
+print(np.ndim(B))
+
+print(B.shape)
